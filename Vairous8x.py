1d36eeb38d35434462886ce8543374ce1539bc7d
Vairous8x <vairous8x1@gmail.com>
2024-11-09 20:47:53 +0300

    Add files via upload

---
+++ Vairous8x.py
@@ -0,0 +1,256 @@
+import os
+os.system("clear")
+print ("\033[32;m")
+os.system ("figlet Tools-Vairous 8x")
+print ("   \033[31;mRoot on Termux       [1]               \033[33;mkali-linux  [5]")
+print ("   \033[31;mHack-Email           [2]               \033[33;mDDos-attack [6]")
+print ("   \033[31;mIp-Target or DNS     [3]               \033[33;mCam-Hackers [7]")
+print ("   \033[31;mInstall Tools-Termux [4]               \033[33;m    ")
+print ("\033[34;m")
+name = input ("    Enter...")
+if name=="1":
+    print ("one")
+    import os
+    os.system ("clear")
+    print ("\033[31;m")
+    os.system ("figlet V8xRoot")
+    print ("\033[34;m")
+    print(" Root [1]")
+    print (" back [99]\033[34;m")
+    name = input ("Enter ...")
+    if name=="99":
+        import os
+        os.system("python back.py")
+    if name == "1":
+
+
+#import os
+#emprator color
+        m = '\033[81;36m'#سمائي
+        E = '\x1b[1;31m'#ابيض
+        G = '\x1b1;32m'#ابيض
+        S = '\x1b[1;33m'#اصفر
+        Z = '\x1b[1;31m'#احمر
+        X = '\x1b[1;33m'#اصفر
+        Z1 = '\x1b[2;31m'#احمر
+        F = '\x1b[2;32m'#اخضر طوخ
+        A = '\x1b[2;39m'#ابيض
+        C = '\x1b[2;35m'#بنفسجي
+        B = '\x1b[2;36m'#اخضر
+        Y = '\x1b[1;34m'#الازرق
+################
+        logo = '''
+        ──────────────────────────────────────────────
+        ─██████████████─██████████████─██████████████─
+        ─██▒▒▒▒▒▒▒▒▒▒██─██▒▒▒▒▒▒▒▒▒▒██─██▒▒▒▒▒▒▒▒▒▒██─
+        ─██▒▒██████▒▒██─██▒▒██████▒▒██─██▒▒██████████─
+        ─██▒▒██──██▒▒██─██▒▒██──██▒▒██─██▒▒██─────────
+        ─██▒▒██──██▒▒██─██▒▒██████▒▒██─██▒▒██████████─
+        ─██▒▒██──██▒▒██─██▒▒▒▒▒▒▒▒▒▒██─██▒▒▒▒▒▒▒▒▒▒██─
+        ─██▒▒██──██▒▒██─██▒▒██████▒▒██─██████████▒▒██─
+        ─██▒▒██──██▒▒██─██▒▒██──██▒▒██─────────██▒▒██─
+        ─██▒▒██████▒▒██─██▒▒██──██▒▒██─██████████▒▒██─
+        ─██▒▒▒▒▒▒▒▒▒▒██─██▒▒██──██▒▒██─██▒▒▒▒▒▒▒▒▒▒██─
+        ─██████████████─██████──██████─██████████████─
+        ──────────────────────────────────────────────'''
+        os.system('clear')
+        print(logo)
+        print(X+'━─━─━─━─◈─━─━─━─━'*3)
+        print(B+'.               #   Vairous8x  #')
+        print(' ')
+        print(B+'.               #  Password = Vairous8x         #')
+        print(F+'━─━─━─━─◈─━─━─━─━'*3)
+        print('')
+        pas = input(B+'َ         pass ==> ')
+        if pas == 'Vairous8x' :
+            os.system('apt-get update -y')
+            os.system('apt-get upgrade -y')
+            os.system('pkg install proot-distro')
+            os.system('proot-distro install ubuntu')
+            os.system('clear')
+            logoroot = '''
+        ██████╗░░█████╗░░█████╗░████████
+        ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝
+        ██████╔╝██║░░██║██║░░██║░░░██║░░░
+        ██╔══██╗██║░░██║██║░░██║░░░██║░░░
+        ██║░░██║╚█████╔╝╚█████╔╝░░░██║░░░
+        ╚═╝░░╚═╝░╚════╝░░╚════╝░░░░╚═╝░░░'''
+            print(B+logoroot)
+            print(X+'━─━─━─━─◈─━─━─━─━'*3)
+            print(X+'━─━─━─━─◈─━─━─━─━'*3)
+            print('')
+            print(F+'َ             Run the root again, write it')
+            print('')
+            print(X+'  #         proot-distro login ubuntu  #')
+            print('')
+            print(X+'━─━─━─━─◈─━─━─━─━'*3+F)
+            os.system('proot-distro login ubuntu')
+if name=="2":
+    import os
+    os.system("clear")
+    os.system("pkg install figlet")
+    os.system("pkg install python")
+    os.system("pkg install python2")
+    print ("\x1b[2;31mhack-email")
+    os.system("clear")
+    os.system("figlet HACK-EMAIL")
+    print ("\x1b[1;34mhack-email [1]")
+    print ("back [99]\x1b[1;33m")
+    name=input ("Enter...")
+    if name == "1":
+        os.system("python2 hack.py")
+    if name == "99":
+        import os
+        os.system("python back.py")
+
+if name=="3":
+    print ("Vairous8x")
+    import os
+    os.system("clear")
+    os.system("figlet ip-Target")
+    print ("         ")
+    print ("ip-Target or DNS [1]")
+    print ("back [99]")
+    name = input ("Enter...")
+    if name=="99":
+        import os
+        os.system("python back.py")
+    if name=="1":
+        import os
+        import socket
+        import sys
+        os.system("clear")
+        os.system ("pkg install toilet -y ")
+        os.system ("clear")
+        os.system ("toilet -f mono12 -F gay Vairous8x")
+        print ("\x1b[1;33mEnter Your Dns or Target: \x1b[1;34m")
+        hostname=input()
+        ip=socket.gethostbyname(hostname)
+        print ("\x1b[2;31mHost Name Is : ",hostname,'/n' 'Target Ip Is',ip)
+
+
+
+if name=="4":
+    import os
+    os.system("python install.py")
+
+if name=="5":
+    import os
+    print ("\033[32;m")
+    os.system("clear")
+    os.system("figlet kali linux")
+    print ("                 ")
+    print ("\033[31;mkalinethunter [1]")
+    print ("back  [99] ")
+    print ("\033[33;m")
+    name=input("Enter...")
+    if name == "99":
+        import os
+        os.system("python back.py")
+    if name == "1":
+        os.system("apt update")
+        os.system("pkg install wget")
+        os.system("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
+        os.system("chmod +x install-nethunter-termux")
+        os.system("./install-nethunter-termux")
+        os.system("chmod +x *")
+        os.system("wget -O install-nethunter-termux https://offs.ec/2MceZWr && chmod +x install-nethunter-termux && ./install-nethunter-termux")
+
+if name=="6":
+    import os
+    os.system("pkg install python")
+    os.system("pkg install python2")
+    os.system("chmod +x hammer.py")
+    os.system("clear")
+    os.system("figlet DDOS-attack")
+    print ("         ")
+    print ("\033[33;m")
+    print ("Enter the IP of the site....")
+    print ("\033[31;m")
+    n=input()
+    os.system ("python3 hammer.py -s "+n)
+
+
+
+if name=="7":
+    import os
+    os.system("apt-get install python3")
+    os.system ("apt-get install tar")
+    os.system ("pip3 install requests")
+    os.system("pip3 install colorama")
+    os.system("clear")
+    #!/usr/bin/env python3
+
+    import requests, re , colorama ,random
+    from requests.structures import CaseInsensitiveDict
+    colorama.init()
+
+    url = "http://www.insecam.org/en/jsoncountries/"
+
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+    headers["Cache-Control"] = "max-age=0"
+    headers["Connection"] = "keep-alive"
+    headers["Host"] = "www.insecam.org"
+    headers["Upgrade-Insecure-Requests"] = "1"
+    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+
+
+    resp = requests.get(url, headers=headers)
+
+    data = resp.json()
+    countries = data['countries']
+
+    print("""
+    \033[1;31m\033[1;37m ██████╗ █████╗ ███╗   ███╗      ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗ ███████╗
+    ██╔════╝██╔══██╗████╗ ████║      ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗██╔════╝
+    ██║     ███████║██╔████╔██║█████╗███████║███████║██║     █████╔╝ █████╗  ██████╔╝███████╗
+    ██║     ██╔══██║██║╚██╔╝██║╚════╝██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗╚════██║
+    ╚██████╗██║  ██║██║ ╚═╝ ██║      ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║███████║
+    \033[1;31m ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝      ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝
+    \033[1;31m                                                                        ANGELSECURITYTEAM \033[1;31m\033[1;37m""")
+
+    for key, value in countries.items():
+        print(f'Code : ({key}) - {value["country"]} / ({value["count"]})  ')
+        print("")
+
+
+
+    try:
+   
+
+        country = input("Code(##) : ")
+        res = requests.get(
+            f"http://www.insecam.org/en/bycountry/{country}", headers=headers
+        )
+        last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
+
+        for page in range(int(last_page)):
+            res = requests.get(
+                f"http://www.insecam.org/en/bycountry/{country}/?page={page}",
+                headers=headers
+            )
+            find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+    
+            with open(f'{country}.txt', 'w') as f:
+              for ip in find_ip:
+                  print("")
+                  print("\033[1;31m", ip)
+                  f.write(f'{ip}\n')
+    except:
+        pass
+    finally:
+        print("\033[1;37m")
+        print('\033[37mSave File :'+country+'.txt')
+
+        exit()
+
+
+#if name=="8":
+#    import os
+#    os.system("python install.py")
+
+
+
+else:
+    print (" ")
---
+++ install.py
@@ -0,0 +1,98 @@
+import os
+os.system ("clear")
+print ("\033[34;m")
+os.system ("figlet install Tools on Termux")
+print ("\033[31;m")
+print ("install  nmap                    [1]")
+print ("install python2                  [2]")
+print ("install python3                  [3]")
+print ("install  php                     [4]")
+print ("install cmatrix                  [5]")
+print ("install  git                     [6]")
+print ("install  wget                    [7]")
+print ("install  ruby                    [8]")
+print ("install   go                     [9]")
+print ("install   C                      [10]")
+print ("install figlet                   [11]")
+print ("install neofetch                 [12]")
+print ("install metasploit               [13]")
+print ("install All the basics of Termux [14]")
+print (".......................")
+print (" \033[34;m      back   [99]")
+print ("\033[33;m")
+name=input ("Enter...")
+if name == "1":
+    import os
+    os.system ("pkg install nmap")
+    os.system ("python back.py")
+if name == "2":
+    import os
+    os.system ("pkg install python2")
+    os.system ("python back.py")
+if name == "3":
+    import os
+    os.system ("pkg install python")
+    os.system ("python back.py")
+if name == "4":
+    import os
+    os.system ("pkg install php -y")
+    os.system ("python back.py")
+if name == "5":
+    import os
+    os.system ("pkg install cmatrix")
+    os.system ("python back.py")
+if name == "6":
+    import os
+    os.system ("pkg install git")
+    os.system ("python back.py")
+if name == "7":
+    import os
+    os.system ("pkg install wget")
+    os.system ("python back.py")
+if name == "8":
+    import os
+    os.system ("pkg install ruby -y")
+    os.system ("python back.py")
+if name == "9":
+    import os
+    os.system ("apt install golang -y")
+    os.system ("python back.py")
+if name == "10":
+    import os
+    os.system ("apt install clang -y")
+    os.system ("python back.py")
+if name == "11":
+    import os
+    os.system ("pkg install figlet")
+    os.system ("python back.py")
+if name == "13":
+    import os
+    os.system ("apt update")
+    os.system ("N")
+    os.system ("apt upgrade -y")
+    os.system ("pkg install wget curl openssh git -y")
+    os.system ("apt install ncurses-utils")
+    os.system ("wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh")
+    os.system("chmod +x metasploit.sh && ./metasploit.sh")
+    os.system ("msfconsole")
+    os.system ("python back.py")
+if name == "12":
+    import os
+    os.system ("pkg install neofetch")
+    os.system ("python back.py")
+
+if name=="14":
+    import os
+    os.system("clear")
+    os.system("termux-setup-storage;cd;dpkg --configure -a;pkg update -y;pkg upgrade -y;pkg install python -y;pkg install python2 -y;pkg install python2-dev -y;pkg install python3 -y;pkg install pip -y;pkg install pip2;pip2 install requests;pkg install fish -y;pkg install ruby -y;pkg install git -y;pkg install dnsutils -y;pkg install php -y;pkg install perl -y;pkg install nmap -y;pkg install bash -y;pkg install clang -y;pkg install nano -y;pkg install w3m -y;pkg install figlet -y;pkg install cowsay -y;gem install lolcat;pkg install curl -y;pkg install tar -y;pkg install zip -y;pkg install unzip -y;pkg install tor -y;pkg install wget -y;pkg install wgetrc -y;pkg install wcalc -y;pkg install bmon -y;pkg install unrar -y;pkg install toilet -y;pkg install proot -y;pkg install golang -y;pkg install chroot -y;termux-chroot -y;pkg install openssl -y;pkg install cmatrix -y;pkg install openssh -y;apt update && apt upgrade -y")
+    os.system ("python back.py")
+
+
+
+
+
+
+
+if name == "99":
+    import os
+    os.system ("python back.py")
